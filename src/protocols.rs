1fa58742bf61fcbe8a4df2ea0462e12e025ae947
tgrandjean <thibault@cornet-grandjean.com>
2026-08-12 15:40:38 +0200

    feat(release): publish ThreadWeave to crates.io

--- src/protocols.rs
+++ src/protocols.rs
@@ -1,27 +1,25 @@
 pub mod common {
     pub mod v1 {
-        pub use threadweave_protocols_prost::threadweave_protocols::common::v1::*;
+        pub use threadweave_protocols::threadweave_protocols::common::v1::*;
     }
 }
 pub mod artifacts {
     pub mod v1 {
-        pub use threadweave_protocols_prost::threadweave_protocols::artifacts::v1::*;
+        pub use threadweave_protocols::threadweave_protocols::artifacts::v1::*;
     }
 }
 pub mod execution {
     pub mod v1 {
-        pub use threadweave_protocols_prost::threadweave_protocols::execution::v1::*;
-        pub use threadweave_protocols_tonic::threadweave_protocols::execution::v1::tonic::*;
+        pub use threadweave_protocols::threadweave_protocols::execution::v1::*;
     }
 }
 pub mod runtime {
     pub mod v1 {
-        pub use threadweave_protocols_prost::threadweave_protocols::runtime::v1::*;
-        pub use threadweave_protocols_tonic::threadweave_protocols::runtime::v1::tonic::*;
+        pub use threadweave_protocols::threadweave_protocols::runtime::v1::*;
     }
 }
 pub mod broker {
     pub mod v1 {
-        pub use threadweave_protocols_prost::threadweave_protocols::broker::v1::*;
+        pub use threadweave_protocols::threadweave_protocols::broker::v1::*;
     }
 }
